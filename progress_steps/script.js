84a86e6102aa2a9bd0682ae2613e1b0e02842d77
Dhruvang <dhruvang.jhaveri@gmail.com>
2021-03-01 01:18:21 +0530

    Progress Steps

---
+++ progress_steps/script.js
@@ -0,0 +1,62 @@
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
+let currentActive = 1
+
+next.addEventListener('click', () => {
+    currentActive++;
+
+    if(currentActive > circles.length)
+    {
+        currentActive = circles.length
+    }
+
+    UpdateProgress()
+})
+
+prev.addEventListener('click', () => {
+    
+
+    if(currentActive > 1)
+    {
+        currentActive--;
+    }
+
+    UpdateProgress()
+})
+
+function UpdateProgress() {
+
+    circles.forEach((circle, index) => {
+        if(index < currentActive)
+        {
+            circle.classList.add('active')
+        }
+        else
+        {
+            circle.classList.remove('active')
+        } 
+
+    })
+
+    if(currentActive === 1)
+    {
+        prev.disabled = true;
+    }
+    else if(currentActive === circles.length)
+    {
+        next.disabled = true;
+    }
+    else
+    {
+        prev.disabled = false;
+        next.disabled = false;
+    }
+
+
+
+    const actives = document.querySelectorAll('.active').length;
+    
+    progress.style.width = (currentActive-1)/(circles.length-1) * 100 + '%'
+}
